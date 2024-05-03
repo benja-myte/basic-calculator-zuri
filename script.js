df5f76921ace789a0b67f9c6c0fa538f698b6653
benja-myte <benjaminudeh236@gmail.com>
2024-05-03 17:48:51 +0100

    Added the files

---
+++ script.js
@@ -0,0 +1,21 @@
+let result = document.getElementById('result');
+
+function appendToResult(value) {
+    result.value += value;
+}
+
+// function clearResult() {
+//     result.value = '';
+// }
+
+function operation(op) {
+    result.value += op;
+}
+
+function calculate() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
